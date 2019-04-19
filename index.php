296c6c4bb5571b83fd141839d4b1763049968570
KhanhPK <phungkhackhanh.pkk@gmail.com>
2019-04-19 17:42:00 +0900

    asdf

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
 <html>
 	<head>
 		<title>Khánh Yêu Hà</title>
+		<style type="text/css">
+			h1 {
+				color: red;
+				margin-top: 300px;
+			}
+		</style>
 	</head>
 	<body>
 		<center>
-			<h1 font-color="red" margin-top="300px">ANH YÊU EM NHIỀU NẠ</h1>
+			<h1>ANH YÊU EM NHIỀU NẠ</h1>
 		</center>
 	</body>
 </html>
